838e43b031660327d2b1e80b890e294941ea09c6
George S. Baugh <george@troglodyne.net>
2014-08-07 19:24:06 -0500

    Update doc

--- lib/TestLink/API.pm
+++ lib/TestLink/API.pm
@@ -1,6 +1,6 @@
 package TestLink::API;
 {
-    $TestLink::API::VERSION = '0.005';
+    $TestLink::API::VERSION = '0.006';
 }
 
 
@@ -1488,8 +1488,14 @@ L<XMLRPC::Lite>
 
 =head1 AUTHOR
 
-George Baugh (george@troglodyne.net)
+George Baugh <teodesian@cpan.org>
 
 =head1 SPECIAL THANKS
 
 cPanel, Inc. graciously funded the initial work on this project.
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by George S. Baugh.
+
+This is free software; you can redistribute it and/or modify it under the same terms as the Perl 5 programming language system itself.
